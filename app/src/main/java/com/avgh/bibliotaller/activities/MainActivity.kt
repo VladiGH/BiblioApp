8ecbae19438e175ab3d499201576f59b323d4cdc
alvarogarcia1010 <00111317@uca.edu.sv>
2019-05-21 21:28:31 -0600

    Agregando vista base en landscape

--- app/src/main/java/com/avgh/bibliotaller/activities/MainActivity.kt
+++ app/src/main/java/com/avgh/bibliotaller/activities/MainActivity.kt
@@ -1,17 +1,12 @@
-package com.avgh.bibliotaller
+package com.avgh.bibliotaller.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
+import com.avgh.bibliotaller.R
 import com.avgh.bibliotaller.room.LibraryDatabase
-import com.avgh.bibliotaller.room.entities.Book
 import com.avgh.bibliotaller.room.entities.Content
-import kotlinx.coroutines.delay
-import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
 
